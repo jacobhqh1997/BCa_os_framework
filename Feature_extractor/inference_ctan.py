26e0543033daf2e749647f1de950e758d91acf0d
jacob_he <148776941+jacobhqh1997@users.noreply.github.com>
2024-08-27 19:53:09 +0800

    Update inference_ctan.py

--- Feature_extractor/inference_ctan.py
+++ Feature_extractor/inference_ctan.py
@@ -54,34 +54,34 @@ class ImageFeatureDataset(Dataset):
             image = self.transform(image)
         return image, (x, y), img_path
 
-# 初始化变量和目录
+
 image_dir = 'path/to/patch_img/'
 output_dir = 'path/to/save_img/'
 os.makedirs(output_dir, exist_ok=True)
 
-# 设置转换
+
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
 ])
 
-# 加载数据集
+
 dataset = ImageFeatureDataset(image_dir, transform)
 dataloader = DataLoader(dataset, batch_size=1000, shuffle=False, num_workers=0)
 
-# 初始化模型
+
 model = ctranspath()
 model.head = nn.Identity()
 td = torch.load('path/to/ctranspath.pth')
 model.load_state_dict(td['model'], strict=False)
 
-# 设置设备
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 model.eval()
 
-# 存储特征和坐标
+
 features_dict = defaultdict(lambda: {'features': [], 'coords': []})
 
 with torch.no_grad():
@@ -94,7 +94,7 @@ with torch.no_grad():
             features_dict[subdir]['features'].append(feature.cpu())
             features_dict[subdir]['coords'].append((y.item(), x.item())) 
 
-# 保存特征和坐标到文件
+
 for subdir, data in features_dict.items():
     filename = f"{subdir}_features.pt"
     result_path = os.path.join(output_dir, filename)
